1fdc32d0fe1d6dd278c414c380d981c35926ff9c
David Kraemer <david.kraemer@stonybrook.edu>
2018-04-04 20:51:37 -0400

    Updated main.

--- polygonal-niceness/main.cpp
+++ polygonal-niceness/main.cpp
@@ -1,18 +1,24 @@
-#include <iostream>
-#include <CGAL/Simple_cartesian.h>
-
-typedef CGAL::Simple_cartesian<double> Kernel;
-typedef Kernel::Point_2 Point2D;
-typedef Kernel::Segment_2 Segment2D;
+#include "polygon2d.h"
 
 int main() {
-  Point2D p(1,1), q(10,10);
+  for (int n = 8; n < 9; n++) {
+    Point_2 points[n];
+    for (int i = 0; i < n; i++)
+      points[i] = Point_2(std::cos(i * 2*pi<double> / n), std::sin(i * 2*pi<double> / n));
 
-  std::cout << "p = " << p << std::endl;
-  std::cout << "q = " << q.x() << " " << q.y() << std::endl;
+    Polygon2D pol(Polygon_2(points, points+n));
 
-  std::cout << "sqdist(p,q) = " 
-    << CGAL::squared_distance(p,q) << std::endl;
+    auto delta = 0.5;
+
+    std::cout << pol[0] << std::endl;
+
+    std::cout << "Area: " << pol.area() << std::endl;
+    std::cout << "Convex?: " << pol.is_convex() << std::endl;
+    std::cout << "Simple?: " << pol.is_simple() << std::endl;
+    std::cout << delta << "-refined?: " << pol.is_refined(delta*delta) << std::endl;
+    // pol.refine(delta*delta);
+
+  }
 
   return 0;
 
