63c6ee05e74e8a7e7f3d89610182a56837aac68c
David Kraemer <david.kraemer@stonybrook.edu>
2018-04-17 09:27:13 -0400

    Chord-f-score
    Chord-f-score is fully implemented, barring bugs and testing. I need to document that I took a lot of code from O'Rourke's book.

--- polygonal-niceness/polygon2d.cpp
+++ polygonal-niceness/polygon2d.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <vector>
 
+#define XOR(lb,rb) (!(lb) ^ !(rb))
+
 Polygon2D refinement_by(Polygon2D &p, double delta)
 {
   auto points = std::vector<Point_2>(p.size());
@@ -139,6 +141,74 @@ double chord_f_score(Polygon2D &p, std::function<double(Polygon2D)> f)
   return min;
 }
 
-bool is_chord(Segment2D seg, Polygon2D p) {
+bool is_chord(Segment2D seg, Polygon2D p) 
+{
+  for (auto i = 0; i < p.size(); i++)
+  {
+    auto j = (i + 1) % p.size();
+    if ((p[i] != seg.source()) && (p[j] != seg.source())
+      && (p[i] != seg.target()) && (p[j] != seg.target())
+        && intersect(seg, Segment2D(p[i], p[j])))
+        {
+          return false;
+        }
+  }
   return true;
+}
+
+bool intersect(Segment2D s1, Segment2D s2) 
+{
+  return intersect_properly(s1, s2) ||
+    is_between(s1, s2.source()) ||
+    is_between(s1, s2.target()) ||
+    is_between(s2, s1.source()) ||
+    is_between(s2, s1.target());
+}
+
+bool is_between(Segment2D seg, Point_2 p) 
+{
+  if (!is_collinear(seg, p)) 
+  {
+    return false;
+  }
+
+  if (seg.source().x() != seg.target().x())
+  {
+    return ((seg.source().x() <= p.x()) && (p.x() <= seg.target().x())) || 
+      ((seg.source().x() >= p.x()) && (p.x() >= seg.target().x()));
+  } 
+  else
+  {
+    return ((seg.source().y() <= p.y()) && (p.y() <= seg.target().y())) || 
+      ((seg.source().y() >= p.y()) && (p.y() >= seg.target().y()));
+  }
+}
+
+bool intersect_properly(Segment2D s1, Segment2D s2)
+{
+  if (is_collinear(s1, s2.source()) ||
+      is_collinear(s1, s2.target()) ||
+      is_collinear(s2, s1.source()) ||
+      is_collinear(s2, s1.target()))
+  {
+    return false;
+  }
+
+  return XOR(is_left(s1, s2.source()), is_left(s1, s2.target())) &&
+    XOR(is_left(s2, s1.source()), is_left(s2, s1.target()));
+}
+
+bool is_collinear(Segment2D seg, Point_2 p)
+{
+  return area(seg.source(), seg.target(), p) == 0.;
+}
+
+bool is_left(Segment2D seg, Point_2 p)
+{
+  return area(seg.source(), seg.target(), p) > 0.;
+}
+
+double area(Point_2 p, Point_2 q, Point_2 r)
+{
+  return (double) CGAL::area(p, q, r);
 }
--- polygonal-niceness/polygon2d.h
+++ polygonal-niceness/polygon2d.h
@@ -17,6 +17,9 @@ typedef K::Intersect_2 Intersect_2;
 class Segment2D {
   public:
     Segment2D(Point_2 p1, Point_2 p2) : _seg(p1, p2) {}
+    Point_2 source() const { return _seg.source(); }
+    Point_2 target() const { return _seg.target(); }
+    
   private:
     Segment_2 _seg;
 };
@@ -43,5 +46,12 @@ bool is_chord(Segment2D seg, Polygon2D p);
 
 Polygon2D refinement_by(Polygon2D &p, double delta_sqr);
 double chord_f_score(Polygon2D &p, std::function<double(Polygon2D)> f);
+double area2(Point_2 p, Point_2 q, Point_2 r);
+bool is_left(Segment2D seg, Point_2 p);
+bool is_collinear(Segment2D seg, Point_2 p);
+bool intersect_properly(Segment2D s1, Segment2D s2);
+bool is_between(Segment2D seg, Point_2 p);
+bool intersect(Segment2D s1, Segment2D s2);
+bool is_chord(Segment2D seg, Polygon2D p);
 
 #endif
